399925d5637f33783e4fac09ae3482689d4e10b0
Alvan <142717474+Alvanza0904@users.noreply.github.com>
2026-09-10 21:34:11 +0700

    1

--- app/(public)/model/[slug]/layout.tsx
+++ app/(public)/model/[slug]/layout.tsx
@@ -18,7 +18,7 @@ interface ModelLayoutProps {
 }
 
 export async function generateStaticParams() {
-  return getModelSlugs().map((slug) => ({ slug }));
+  return (await getModelSlugs()).map((slug) => ({ slug }));
 }
 
 export default async function ModelLayout({ children, params }: ModelLayoutProps) {
