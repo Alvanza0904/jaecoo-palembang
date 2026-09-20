7d793de1aa751aabd41ae76e21059e9bf1933afe
Git Rover User <unknown@local>
2026-09-20 18:14:45 +0700

    Q

--- lib/data/site.ts
+++ lib/data/site.ts
@@ -8,11 +8,11 @@
 
 export const SITE_SETTINGS = {
   brandName: "JAECOO Palembang",
-  dealerName: "Dealer Resmi Omoda Jaecoo Palembang",
+  dealerName: "Dealer Resmi Jaecoo Palembang",
   salesName: "Alvan",
   whatsappNumber: "6285183145926",
-  instagram: "https://instagram.com/jaecoopalembang",
-  tiktok: "https://tiktok.com/@jaecoopalembang",
+  instagram: "https://instagram.com/omodajaecoopalembang_alvan",
+  tiktok: "https://tiktok.com/@omodajaecoopalembang",
   facebook: "https://facebook.com/jaecoopalembang",
   dealerAddress: {
     street: "Komp. Graha Maju, Jl. Mayor HM. Rasyad Nawawi No.506 - 509",
